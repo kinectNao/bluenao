a17f07d8171ee63620d22fb459f2563b013edb8f
Bluenao <bluenao@dhbw-karlsruhe.de>
2014-03-06 16:05:24 +0100

    adding some stuff

---
+++ KinectExplorer-WPF/Nao.cs
@@ -0,0 +1,66 @@
+﻿
+
+namespace Microsoft.Samples.Kinect.KinectExplorer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Aldebaran.Proxies;
+    using System.Collections;
+    using System.Threading;
+
+
+    public class Nao
+    {
+
+        private MotionProxy motion;
+        private RobotPostureProxy rpp;
+        private TextToSpeechProxy tts;
+        private KinectWindow kinectWindow;
+
+        public Nao(KinectWindow kinectWindow)
+        {
+            // TODO: Complete member initialization
+            this.kinectWindow = kinectWindow;
+        }
+
+
+        public void Initialisierung(String ip, Int32 port)
+        {
+            motion = new MotionProxy(ip, port);
+            rpp = new RobotPostureProxy(ip, port);
+            tts = new TextToSpeechProxy(ip, port);
+
+            Nao_Start Nao_Start = new Nao_Start();
+
+
+            //Nao geht in die Startposition
+            Nao_Start.Startposition(rpp, tts);
+        }
+
+        public void Bew_Winkel()
+        {
+            Console.WriteLine("Start des Threads");
+            Nao_Bewegen Nao_Bewegen = new Nao_Bewegen(rpp);
+            Nao_Winkel Nao_Winkel = new Nao_Winkel(motion);
+            Thread[] ta = new Thread[2];
+            if (kinectWindow.Neue_Beweg)
+            {
+                ta[0] = new Thread(Nao_Bewegen.Bewegung_erzeugen);
+            }
+            else
+            {
+                ta[0] = new Thread(Nao_Bewegen.Bewegen);
+            }
+            ta[1] = new Thread(Nao_Winkel.PositionLArm);
+            ta[0].Start();
+            ta[1].Start();
+
+            while (ta[0].IsAlive) ;
+            Nao_Winkel.RequestStop_Winkel();
+            Console.WriteLine("Ende der Threads");
+        }
+    }
+}
---
+++ KinectExplorer-WPF/Nao_Winkel.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Aldebaran.Proxies;
+using System.Threading;
+
+namespace Microsoft.Samples.Kinect.KinectExplorer
+{
+    class Nao_Winkel
+    {
+        private volatile bool StopThread_Winkel;
+        private MotionProxy motion;
+
+
+
+        public Nao_Winkel(MotionProxy motion)
+        {
+            // TODO: Complete member initialization
+            this.motion = motion;
+        }
+
+        public void PositionLArm()
+        {
+            StopThread_Winkel = false;
+            int HaeufigkeitWinkel = 0;
+            while (!StopThread_Winkel)
+            {
+                string name = "LArm";
+                int space = 2;
+                bool useSensorValues = true;
+                List<float> result = new List<float>();
+                result = motion.getPosition(name, space, useSensorValues);
+                HaeufigkeitWinkel++;
+                for (int i = 0; i < 6; i++)
+                {
+                   // Console.WriteLine("LArm: " + HaeufigkeitWinkel + ". Auswertung: Wert an Stelle " + i + " ist:  " + result.ElementAt(i));
+                }
+            }
+        }
+
+        public void RequestStop_Winkel()
+        {
+            StopThread_Winkel = true;
+        }
+
+
+
+        /*
+        public static void PositionRArm()
+        {
+            string name = "RArm";
+            int space = 2;
+            bool useSensorValues = true;
+            List<float> result = new List<float>();
+            //result = motion.getPosition(name, space, useSensorValues);
+            Console.WriteLine("test " + name);
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("RArm: Wert an Stelle " + i + " ist:  " + result.ElementAt(i));
+            }
+        }
+
+        public static void PositionHead()
+        {
+            string name = "Head";
+            int space = 2;
+            bool useSensorValues = true;
+            List<float> result = new List<float>();
+            //result = motion.getPosition(name, space, useSensorValues);
+            Console.WriteLine("test " + name);
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Head: Wert an Stelle " + i + " ist:  " + result.ElementAt(i));
+            }
+        }
+
+        public static void PositionLLeg()
+        {
+            string name = "LLeg";
+            int space = 2;
+            bool useSensorValues = true;
+            List<float> result = new List<float>();
+            //result = motion.getPosition(name, space, useSensorValues);
+            Console.WriteLine("test " + name);
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("LLeg: Wert an Stelle " + i + " ist:  " + result.ElementAt(i));
+            }
+        }
+
+        public static void PositionRLeg()
+        {
+            string name = "RLeg";
+            int space = 2;
+            bool useSensorValues = true;
+            List<float> result = new List<float>();
+            //result = motion.getPosition(name, space, useSensorValues);
+            Console.WriteLine("test " + name);
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("RLeg: Wert an Stelle " + i + " ist:  " + result.ElementAt(i));
+            }
+        }
+
+        public static void PositionTorso()
+        {
+            string name = "Torso";
+            int space = 2;
+            bool useSensorValues = true;
+            List<float> result = new List<float>();
+            //result = motion.getPosition(name, space, useSensorValues);
+            Console.WriteLine("test " + name);
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Torso: Wert an Stelle " + i + " ist:  " + result.ElementAt(i));
+            }
+        }
+         */
+    }
+}
